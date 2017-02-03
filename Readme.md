9badb04830e6e0e11914b44e630a19fc47b2ebc3
Merlin Marek <merlin_marek@web.de>
2017-02-03 13:04:53 +0100

    mathematical evalution moved to generator
    instead of simple functions like cos or sin the generator executable
    can now evaluate more complex mathematical expressions using
    math_eval(https://github.com/merlinmarek/math_eval) as backend.

--- Readme.md
+++ Readme.md
@@ -1,10 +1,17 @@
-Generate random data points for simple functions.
+Generate random data points for mathematical functions.
 The data points are saved in the text-based format used by [fann](https://github.com/libfann/fann) to train neural networks.
 
-build with:
+This project depends on [math_eval](https://github.com/merlinmarek/math_eval).
+
+Build with:
 ```
 mkdir build
 cd build
 cmake ..
 make
+```
+
+Usage:
+```
+generator "<mathematical expression>" <number of samples> <lower limit> <upper limit>
 ```
---
+++ generator/generator.cc
@@ -0,0 +1,51 @@
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <math_eval.h>
+
+int main(int argc, char** argv)
+{
+    if(argc != 5)
+    {
+        std::cout << "Usage: generator \"<mathematical expression>\" <number of samples> <lower limit> <upper limit>" << std::endl;
+        return -1;
+    }
+    std::string math_expr = std::string(argv[1]);
+    int num_samples = std::stoi(std::string(argv[2]));
+    float lower_limit = std::stof(std::string(argv[3]));
+    float upper_limit = std::stof(std::string(argv[4]));
+    float range = upper_limit - lower_limit;
+    
+    if(lower_limit > upper_limit)
+    {
+        std::cout << "lower limit has to be greater than upper limit" << std::endl;
+        return -1;
+    }
+
+    std::ofstream file("fann.data");
+    file << num_samples << " 1 1" << std::endl;
+
+    for(int i = 0; i < num_samples; ++i)
+    {
+        float input = ((float)(rand())) / ((float)RAND_MAX) * range + lower_limit;
+        std::string input_string = std::to_string(input);
+
+        std::string eval_string = math_expr;
+        size_t start_pos = eval_string.find("x");
+        if(start_pos == std::string::npos)
+        {
+            std:: cout << "variable x not found" << std::endl;
+        }
+        if(input >= 0)
+            eval_string.replace(start_pos, 1, input_string); 
+        else
+            eval_string.replace(start_pos, 1, "(" + input_string + ")");
+
+        float output = math_eval(infix_to_postfix(eval_string));
+        //std::cout << eval_string << "\t->\t" << output << std::endl;
+        file << input << std::endl << output << std::endl;
+    }
+    return 0; 
+}
